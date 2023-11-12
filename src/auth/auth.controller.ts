0ccec1d176cf04183c4a4b4fbb30dfd14c2d1231
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2023-11-12 11:25:24 -0500

    jwt implementation

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserDto, CreateUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testing() {
+    return {
+      ok: true,
+      message: 'Hola mundo',
+    };
+  }
+}
